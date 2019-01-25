0b173c2d864cf6ae5b39adde232adcf5df23e28f
tofaelKabir <tofaelkabir@gmail.com>
2019-01-25 16:05:24 -0500

    Transitions

---
+++ UiCatalog/src/test/java/iPhone/TestTransitions.java
@@ -0,0 +1,31 @@
+package iPhone;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestTransitions extends Transitions {
+    Transitions transitions;
+    @BeforeMethod
+    public void navigate(){
+        UiCatalog ui = PageFactory.initElements(ad, UiCatalog.class);
+        ui.getToolbar();
+        transitions = PageFactory.initElements(ad, Transitions.class);
+    }
+    @Test
+    public void testTransitionsPageWhole(){
+        transitions.checkTransitionsPageWhole();
+    }
+    @Test
+    public void testTransitionsPageImage(){
+        transitions.checkTransitionsPageImages();
+    }
+    @Test
+    public void testTransitionsPageFlipImage(){
+        transitions.checkTransitionsPageFlipImages();
+    }
+    @Test
+    public void testTransitionsPageCurlImage(){
+        transitions.checkTransitionsPageCurlImages();
+    }
+}
